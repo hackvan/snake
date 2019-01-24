a8e4881a95155df1f3bd6b7801bdca82f76dc352
Diego Camacho <hackvan@gmail.com>
2019-01-24 13:39:18 -0500

    Grow snake when eat food

--- test/actions_test.rb
+++ test/actions_test.rb
@@ -67,4 +67,25 @@ class ActionsTest < Minitest::Test
 
     assert_equal actual_state, expected_state
   end
+
+  def test_snake_grow
+    initial_state = Model::State.new(
+      Model::Snake.new([
+        Model::Coord.new(1, 1),
+        Model::Coord.new(0, 1)
+      ]),
+      Model::Food.new(2, 1),
+      Model::Grid.new(8, 12),
+      Model::Direction::DOWN,
+      false
+    )
+
+    actual_state = Actions::move_snake(initial_state)
+
+    assert_equal(actual_state.snake.positions, [
+      Model::Coord.new(2, 1),
+      Model::Coord.new(1, 1),
+      Model::Coord.new(0, 1)
+    ])
+  end
 end
